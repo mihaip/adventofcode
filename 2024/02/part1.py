b95c162eeeed1d683cd1a22e26d3e49887576236
Mihai Parparita <mihai@persistent.info>
2024-12-27 20:49:32 +0100

    2024 Day 2 Part 1
    Pair programmed with Max

---
+++ 2024/02/part1.py
@@ -0,0 +1,42 @@
+#!/usr/local/bin/python3
+
+import sys
+
+def is_safe_increasing(report):
+    for i in range(len(report) - 1):
+        item1 = report[i]
+        item2 = report[i + 1]
+        diff = item2 - item1
+        if diff <= 3 and diff >= 1:
+            # Do nothing
+            pass
+        else:
+            return False
+    return True
+
+def is_safe_decreasing(report):
+    for i in range(len(report) - 1):
+        item1 = report[i]
+        item2 = report[i + 1]
+        diff = item2 - item1
+        if diff >= -3 and diff <= -1:
+            # Do nothing
+            pass
+        else:
+            return False
+    return True
+
+def is_safe(report):
+    return is_safe_decreasing(report) or is_safe_increasing(report)
+
+safe_count = 0
+
+for line in sys.stdin.readlines():
+    report = line.split()
+    report = [int(item) for item in report]
+
+    is_report_safe = is_safe(report)
+    if is_report_safe:
+        safe_count += 1
+
+print(safe_count)
